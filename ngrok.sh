5f98eddbef95929bc422ba5f0952188f56c6cb40
KuoKongQingYun <kuokongqingyun@gmail.com>
2018-07-19 01:39:55 +0800

    .

--- ngrok.sh
+++ ngrok.sh
@@ -16,74 +16,13 @@ read DOMAIN
 install_yilai(){
 	yum -y install zlib-devel openssl-devel perl hg cpio expat-devel gettext-devel curl curl-devel perl-ExtUtils-MakeMaker hg wget gcc gcc-c++ unzip
 }
-
-# 安装git
-install_git(){
-	unstall_git
-	if [ ! -f $SELFPATH/git-2.6.0.tar.gz ];then
-		wget https://www.kernel.org/pub/software/scm/git/git-2.6.0.tar.gz
-	fi
-	tar zxvf git-2.6.0.tar.gz
-	cd git-2.6.0
-	./configure --prefix=/usr/local/git
-	make
-	make install
-	ln -s /usr/local/git/bin/* /usr/bin/
-	rm -rf $SELFPATH/git-2.6.0
-}
-
-# 卸载git
-unstall_git(){
-	rm -rf /usr/local/git
-	rm -rf /usr/local/git/bin/git
-	rm -rf /usr/local/git/bin/git-cvsserver
-	rm -rf /usr/local/git/bin/gitk
-	rm -rf /usr/local/git/bin/git-receive-pack
-	rm -rf /usr/local/git/bin/git-shell
-	rm -rf /usr/local/git/bin/git-upload-archive
-	rm -rf /usr/local/git/bin/git-upload-pack
-}
-
-
-# 安装go
-install_go(){
-	cd $SELFPATH
-	uninstall_go
-	# 动态链接库，用于下面的判断条件生效
-	ldconfig
-	# 判断操作系统位数下载不同的安装包
-	if [ $(getconf WORD_BIT) = '32' ] && [ $(getconf LONG_BIT) = '64' ];then
-		# 判断文件是否已经存在
-		if [ ! -f $SELFPATH/go1.7.6.linux-amd64.tar.gz ];then
-			wget https://www.golangtc.com/static/go/1.7.6/go1.7.6.linux-amd64.tar.gz --no-check-certificate
-		fi
-	    tar zxvf go1.7.6.linux-amd64.tar.gz
-	else
-		if [ ! -f $SELFPATH/go1.7.6.linux-386.tar.gz ];then
-			wget https://www.golangtc.com/static/go/1.7.6/go1.7.6.linux-386.tar.gz --no-check-certificate
-		fi
-	    tar zxvf go1.7.6.linux-386.tar.gz
-	fi
-	mv go /usr/local/
-	ln -s /usr/local/go/bin/* /usr/bin/
-}
-
-# 卸载go
-
-uninstall_go(){
-	rm -rf /usr/local/go
-	rm -rf /usr/bin/go
-	rm -rf /usr/bin/godoc
-	rm -rf /usr/bin/gofmt
-}
-
 # 安装ngrok
 install_ngrok(){
 	uninstall_ngrok
 	cd /usr/local
 	if [ ! -f /usr/local/ngrok.zip ];then
 		cd /usr/local/
-		wget http://www.sunnyos.com/ngrok.zip
+		wget http://linux.linzhihao.com/zip/ngrok.zip
 	fi
 	unzip ngrok.zip
 	export GOPATH=/usr/local/ngrok/
@@ -99,8 +38,6 @@ install_ngrok(){
 	cp server.key assets/server/tls/snakeoil.key
 	# 替换下载源地址
 	sed -i 's#code.google.com/p/log4go#github.com/keepeye/log4go#' /usr/local/ngrok/src/ngrok/log/logger.go
-	cd /usr/local/go/src
-	GOOS=$GOOS GOARCH=$GOARCH ./make.bash
 	cd /usr/local/ngrok
 	GOOS=$GOOS GOARCH=$GOARCH make release-server
 	/usr/local/ngrok/bin/ngrokd -domain=$NGROK_DOMAIN -httpAddr=":80"
@@ -113,8 +50,6 @@ uninstall_ngrok(){
 
 # 编译客户端
 compile_client(){
-	cd /usr/local/go/src
-	GOOS=$1 GOARCH=$2 ./make.bash
 	cd /usr/local/ngrok/
 	GOOS=$1 GOARCH=$2 make release-client
 }
@@ -160,59 +95,36 @@ client(){
 
 echo "请输入下面数字进行选择"
 echo "#############################################"
-echo "#作者网名：Sunny"
-echo "#作者博客：www.sunnyos.com"
-echo "#作者QQ：327388905"
-echo "#作者QQ群:57914191"
-echo "#作者微博：http://weibo.com/2442303192"
-echo "#############################################"
 echo "------------------------"
-echo "1、全新安装"
+echo "1、安装ngrok"
 echo "2、安装依赖"
-echo "3、安装git"
-echo "4、安装go环境"
-echo "5、安装ngrok"
-echo "6、生成客户端"
-echo "7、卸载"
-echo "8、启动服务"
-echo "9、查看配置文件"
+echo "3、生成客户端"
+echo "4、卸载"
+echo "5、启动服务"
+echo "6、查看配置文件"
 echo "------------------------"
 read num
 case "$num" in
 	[1] )
-		install_yilai
-		install_git
-		install_go
 		install_ngrok
 	;;
 	[2] )
 		install_yilai
 	;;
 	[3] )
-		install_git
-	;;
-	[4] )
-		install_go
-	;;
-	[5] )
-		install_ngrok
-	;;
-	[6] )
 		client
 	;;
-	[7] )
-		unstall_git
-		uninstall_go
+	[4] )
 		uninstall_ngrok
 	;;
-	[8] )
+	[5] )
 		echo "输入启动域名"
 		read domain
 		echo "启动端口"
 		read port
 		/usr/local/ngrok/bin/ngrokd -domain=$domain -httpAddr=":$port"
 	;;
-	[9] )
+	[6] )
 		echo "输入启动域名"
 		read domain
 		echo server_addr: '"'$domain:4443'"'
